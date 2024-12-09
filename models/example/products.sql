14a52fdc6c4816d7123acd6449bb9030100f2b82
Samith Hegde <samith.hegde@bizacuity.com>
2024-12-09 06:44:06 +0000

    modified products.sql

--- models/example/products.sql
+++ models/example/products.sql
@@ -17,13 +17,16 @@ WITH new_data AS (
         A.create_timestamp,
         A.update_timestamp,
         1001 AS etl_batch_no,  -- Pass in ETL batch number via variable
-        '2001-01-01' AS etl_batch_date -- Pass in ETL batch date via variable
+        '2001-01-01' AS etl_batch_date,  -- Pass in ETL batch date via variable
+        ROW_NUMBER() OVER (ORDER BY A.productCode) + COALESCE(MAX(existing_data.dw_product_id) OVER (), 0) AS dw_product_id
     FROM {{ source('devstage', 'Products') }} A
     JOIN {{ ref('productlines') }} PL ON A.productLine = PL.productLine
+    LEFT JOIN {{ this }} existing_data ON A.productCode = existing_data.src_productCode
 )
 
 -- The main insert/select for incremental load
-SELECT * FROM new_data
+SELECT * 
+FROM new_data
 
 {% if is_incremental() %}
     WHERE src_productCode NOT IN (SELECT src_productCode FROM {{ this }})
