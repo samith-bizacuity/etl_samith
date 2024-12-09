0c07f86f308a509e97c0c888f1c8a5b93234acf3
Samith Hegde <samith.hegde@bizacuity.com>
2024-12-09 06:46:44 +0000

    eq

--- models/example/products.sql
+++ models/example/products.sql
@@ -14,8 +14,8 @@ WITH new_data AS (
         A.buyPrice,
         A.MSRP,
         PL.dw_product_line_id,
-        A.create_timestamp,
-        A.update_timestamp,
+        A.create_timestamp as src_create_timestamp,
+        A.update_timestamp as src_update_timestamp,
         1001 AS etl_batch_no,  -- Pass in ETL batch number via variable
         '2001-01-01' AS etl_batch_date,  -- Pass in ETL batch date via variable
         ROW_NUMBER() OVER (ORDER BY A.productCode) + COALESCE(MAX(existing_data.dw_product_id) OVER (), 0) AS dw_product_id
