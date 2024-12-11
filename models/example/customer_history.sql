e6ea51a7d05f10f2df000a0fcf297ed98950fdf2
Samith Hegde <samith.hegde@bizacuity.com>
2024-12-11 07:33:38 +0000

    jk

--- models/example/customer_history.sql
+++ models/example/customer_history.sql
@@ -8,11 +8,12 @@ with new_customers as (
     select
         C.dw_customer_id,
         C.creditLimit,
-        {{ source('etl_metadata', 'batch_control') }}.etl_batch_no,
-        {{ source('etl_metadata', 'batch_control') }}.etl_batch_date
+        B.etl_batch_no,
+        B.etl_batch_date
     from
         {{ ref('customers') }} C
     left join {{ this }} ch on C.dw_customer_id = ch.dw_customer_id
+    cross join {{ source('etl_metadata', 'batch_control') }} B
     where ch.dw_customer_id is null or ch.dw_active_record_ind = 1
 ),
 
@@ -72,11 +73,11 @@ union all
 select
     dw_customer_id,
     creditLimit,
-    {{ source('etl_metadata', 'batch_control') }}.etl_batch_date as effective_from_date,
+    etl_batch_date as effective_from_date,
     null as effective_to_date,
     1 as dw_active_record_ind,
-    {{ source('etl_metadata', 'batch_control') }}.etl_batch_no as create_etl_batch_no,
-    {{ source('etl_metadata', 'batch_control') }}.etl_batch_date as create_etl_batch_date,
+    etl_batch_no as create_etl_batch_no,
+    etl_batch_date as create_etl_batch_date,
     null as update_etl_batch_no,
     null as update_etl_batch_date,
     null as dw_update_timestamp
